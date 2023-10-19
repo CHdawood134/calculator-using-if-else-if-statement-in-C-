92c6b09497b7e1577ad7c3ddb7ba0b21fbee8a15
CHdawood134 <148341065+CHdawood134@users.noreply.github.com>
2023-10-19 11:28:15 +0500

    calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,31 @@
+#inlude<iostream>
+using namespae std;
+int main (){
+	int a,b,;
+	har opr;
+	out<<"enter the value of a"<<endl;
+	in>>a;
+	out<<"enter the value of b"<<endl;
+	in>>b;
+	out<<"enter the operation(+,-,*and/):";
+	in>>opr;
+	
+	if(opr == '+'){
+		=a+b;
+		out<<;
+	}
+	 else if (opr == '-'){
+		=a-b;
+		out<<;
+	}
+	else if(opr == '*'){
+		=a*b;
+		out<<;}
+	if (opr == '/'){
+		=a/b;
+		out<<;
+	}
+	
+	return 0;
+
+}
