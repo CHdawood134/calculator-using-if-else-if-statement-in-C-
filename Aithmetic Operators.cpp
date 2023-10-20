696135a329b3947f32fb2472ecaa43ffeef39021
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-10-20 16:04:03 +0500

    Add files via upload

---
+++ Aithmetic Operators.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a=8 ,b=4;
+	
+	cout<<"Operators in c++"<<endl;
+	
+	cout<<" following are the Operater in c++"<<endl;
+	
+	//Arithmetic Operators
+	
+	cout<<" the value of a+b is :"<<a+b<<endl;
+	
+	
+	cout<<" the value of a-b is :"<<a-b<<endl;
+	
+	
+	cout<<" the value of a*b is :"<<a*b<<endl;
+	
+	cout<<"the value of a/b is:"<<a/b<<endl;
+	
+	cout<<" the value of a%b is :"<<a%b<<endl;
+
+
+	
+	
+	cout<<" the value of a++ is :"<<a++<<endl;
+	
+	
+	
+	cout<<" the value of a-- is :"<<a--<<endl;
+	
+	
+	
+	cout<<" the value of ++a is :"<<++a<<endl;
+	
+	
+	
+	cout<<" the value of --a is :"<<--a<<endl;
+	
+	system ("pause");
+	
+	return 0;
+}
