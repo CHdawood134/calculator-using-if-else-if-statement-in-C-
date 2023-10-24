7523fd28cbf93dbf9e5e1bc0cdb53a9351ac466a
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-10-24 13:32:21 +0500

    Add files via upload

---
+++ Table.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int i ,n;
+	cout<<"Table :"<<endl;
+	cout<<"Enter any number:"<<endl;
+	cin>>n;
+	for(i=1;i<=10;i++){
+	
+	
+	
+		cout<<"\n"<<n<<"  * "<<i<<" = "<<n*i;
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
