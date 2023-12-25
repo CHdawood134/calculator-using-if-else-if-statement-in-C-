e79772f3624591837e88c3efc584fdcaa18b9df4
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-25 22:44:30 +0500

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+// ********** typecasting **********
+// conversion of one data type to another data type.
+int y=49;
+cout<<"the value of y is:"<<(float)	y<<endl;
+float w=34.49;
+cout<<" the value of w is:"<<(int)w;
+
+	 system("pause");	
+	
+	return 0;
+}
