feba831d36ec782d736008e00a974a4038ac3cc2
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-25 21:55:29 +0500

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1;
+	int square;
+	cout<<"please enter num1"<<endl;
+	cin>>num1;
+	square=num1*num1;
+	cout<<"the square of num1 is "<<square;
+	
+	system("pause");
+	
+	return 0;
+}
+
