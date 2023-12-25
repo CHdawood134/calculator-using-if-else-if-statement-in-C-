306c359cc62e70f2cda04ffe36455d2e980bd356
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-25 22:16:17 +0500

    Add files via upload

---
+++ cube and square at a time of number enter.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+using namespace std;
+int main(){
+	int num1;
+	int cube;
+	int square;
+	cout<<"please enter num1"<<endl;
+	cin>>num1;
+	cube=num1*num1*num1;
+	square=num1*num1;
+	cout<<"square of given num1 is:"<<square<<endl;
+	
+	cout<<"cube of given num1 is:"<<cube;
+	system("pause");
+	
+	
+	return 0;
+}
