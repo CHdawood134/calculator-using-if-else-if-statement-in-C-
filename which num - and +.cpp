890d90fcdbfb80717ed2c2f264377e0036bf8294
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-31 13:30:54 +0500

    Add files via upload

---
+++ which num - and +.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num;
+	cout<<"enter the number:"<<endl;
+cin>>num;
+if (num>=0){
+	cout<<"the number is postive"<<endl;
+}
+else{
+	cout<<"the number is negative";
+}
+
+	system("pause");
+	
+	return 0;
+}
