aacc3b1bf48007a0df12cfa62682332f32bc20d3
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-31 16:22:12 +0500

    Add files via upload

---
+++ number is multiple of two.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num;
+	cout<<"enter number:"<<endl;
+	cin>>num;
+	if (num%2==0){
+		cout<<"the number is multiple of two";
+	}
+	else{
+		cout<<"the number is not a multiple of two";
+	}
+	system("pause");	
+	return 0;
+}
