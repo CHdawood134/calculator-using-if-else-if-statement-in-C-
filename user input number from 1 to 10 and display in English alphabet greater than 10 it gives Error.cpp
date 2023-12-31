151d5b5cf9f8461fdd90260a5b97497c48866cd7
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-31 23:01:03 +0500

    Add files via upload

---
+++ user input number from 1 to 10 and display in English alphabet greater than 10 it gives Error.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"please enter number:"<<endl;
+	cin>>num;
+	if(num==1){
+		cout<<"the number is one";
+	}
+	else if(num==2){
+		cout<<"the number is two";
+	}
+		else if(num==3){
+		cout<<"the number is three";
+	}
+		else if(num==4){
+		cout<<"the number is four";
+	}
+		else if(num==5){
+		cout<<"the number is five";
+	}
+		else if(num==6){
+		cout<<"the number is six";
+	}
+		else if(num==7){
+		cout<<"the number is seven";
+	}
+		else if(num==8){
+		cout<<"the number is eight";
+	}
+		else if(num==9){
+		cout<<"the number is nine";
+	}
+		else if(num==10){
+		cout<<"the number is ten";
+	}
+		else
+		{
+		cout<<"Error";
+	}
+	system("pause");
+	return 0;
+}
