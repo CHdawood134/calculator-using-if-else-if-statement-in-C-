6ec1200a2fc11374a5c1ca484a9508e1d9006e46
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-31 14:01:57 +0500

    Add files via upload

---
+++ which num is greater.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int num1;
+	int num2;
+	cout<<"enter first number:"<<endl;
+	cin>>num1;
+	cout<<"enter second number"<<endl;
+	
+	cin>>num2;
+	if(num1>num2){
+		cout<<"the first number is greater than second number";
+	}
+	else{
+		cout<<"the second number is greater than first number";
+		
+	}
+	system("pause");
+	
+	return 0;
+}
