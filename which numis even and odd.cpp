9030a4643605cc8a4f9a1ed47837c4894d789e60
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2023-12-31 13:50:35 +0500

    Add files via upload

---
+++ which numis even and odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+int num;
+cout<<"enter number:"<<endl;
+cin>>num;
+if(num%2==0){
+	cout<<"the number is even "<<endl;
+}
+else{
+	cout<<"the number is odd";
+}
+system("pause");	
+
+	return 0;
+	
+}
