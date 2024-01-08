a1f3224e6506c8b6ff0a9db64c8e839105018a15
Muhammad Dawood <148341065+Dawood134@users.noreply.github.com>
2024-01-08 13:57:24 +0500

    Add files via upload

---
+++ user input string if it is hello than display1 else 0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string x;
+	cout<<"Enter string:"<<endl;
+	cin>>x;
+	int result=(x=="hello")?1:0;
+	cout<<"Result:"<<result<<endl;
+	return 0;
+}
